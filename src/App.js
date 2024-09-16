20b8cb7ec15380cc2e74a8127948421299cdbe50
Gavin P <gavinjpeeler@gmail.com>
2024-09-16 15:50:59 -0700

    randoom button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-
-  let Num = Math.round(Math.random()*10);
+ 
+  const [Num, setNum] = useState(Math.round(Math.random()*10));
+  
+  function refresh() {
+     setNum(Math.round(Math.random()*10));
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. The random number is {Num}
+          Edit <code>src/App.js</code> and save to reload. The random number is <button onClick={refresh}>{Num}</button>
         </p>
         <a
           className="App-link"
