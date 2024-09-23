975161073603da13e45a41edc2ef3d8ac111143b
aaron-elijah-bain <145293925+aaron-elijah-bain@users.noreply.github.com>
2024-09-23 22:54:57 +0000

    Made title big and added spacing

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import './App.css'
 
 function App() {
   const [Num, setNum] = useState(Math.round(Math.random()*10));
+
+  
   
   function refresh() {
       setNum(Math.round(Math.random()*10));
   }  
 
+  
+
   return (
     <>
       <div className="flex flex-col">
-        <h1 className="text-2xl">DVS Bathroom Tracker</h1>
-        <input type = "text" id = "username" name="username" placeholder='Enter Name Here'></input>
+        <div className="mb-10"><h1 className="text-200xl">DVS Bathroom Tracker</h1></div>
+
+        <div className = ""><input type = "text" id = "username" name="username" placeholder='Enter Name Here'></input></div>
+
+        
         <button>Validate Your Pass</button>
-        <button onClick={refresh}>Click here to change the number</button>
+        <button className="mt-14" onClick={refresh}>Click here to change the number</button>
         <p>The number is {Num}</p>
       </div>
     </>
