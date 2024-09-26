1b9e6b8fe6d858d17af218068b9f35296c939b2f
Gavin P <gavinjpeeler@gmail.com>
2024-09-26 09:34:17 -0700

    Made it look more like the wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ function App() {
   return (
     <>
       <div className="flex flex-col">
-        <h1 className="text-200xl mb-10">DVS Bathroom Tracker</h1>
+        <h1 className="text-200xl font-bold mb-10 text-gray-600">DVS Bathroom Tracker</h1>
 
-        <Select className="text-black" value={selectedName} onChange={setSelectedName} options={names} />
-        <button>Validate Your Pass</button>
+        <Select className="text-black border border-black rounded mb-5" value={selectedName} onChange={setSelectedName} options={names} />
+        <button className="text-black bg-green-500 border border-black rounded-md">Validate Your Pass</button>
       </div>
     </>
   )
