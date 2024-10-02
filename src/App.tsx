9c97fa4ca125c9b953bcc834116fa57100b93768
Gavin P <gavinjpeeler@gmail.com>
2024-10-02 15:40:00 -0700

    Whoever maintains react-select consider yourself an opp

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import { useState } from 'react';
-import Select from 'react-select';
+import Select, { ActionMeta, OnChangeValue } from 'react-select';
 import './App.css';
 
-const names = [
-    { value: 'John Cena', label: 'John Cena' },
-    { value: 'Tom Brady', label: 'Tom Brady' }
-];
+type OptionType = {value: string, label: string};
+
+const names: OptionType[] = [
+    { value: 'John Doe', label: 'John Doe' },
+    { value: 'Jane Doe', label: 'Jane Doe' },
+]
 
 function App() {
-  const [selectedName, setSelectedName] = useState(null);
+    const [selectedName, setSelectedName] = useState<OptionType>({} as OptionType);
 
+    const handleChange = (option: OnChangeValue<OptionType,false>, _actionMeta: ActionMeta<OptionType>) => {
+        if (option) {
+            setSelectedName(option);
+        }
+    }  
+  
   return (
     <>
       <div className="flex flex-col">
         <h1 className="text-200xl font-bold mb-10 text-gray-600">DVS Bathroom Tracker</h1>
 
-        <Select className="text-black border border-black rounded mb-5" value={selectedName} onChange={setSelectedName} options={names} />
+        <Select className="text-black border border-black rounded mb-5" value={selectedName} onChange={handleChange} options={names} />
         <button className="text-black bg-green-500 border border-black rounded-md">Validate Your Pass</button>
+        <p>{selectedName.value}</p>
       </div>
     </>
   )
